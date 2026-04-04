7dcddde2e1d17f73947a8ed42edd843f402a73bf
souravprajapati24 <souravprajapati2407@gmail.com>
2026-04-04 21:48:38 +0530

    added openApiDoc/swagger ui config.

---
+++ src/main/java/com/sourav/financedashboardbackend/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.sourav.financedashboardbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Finance API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
